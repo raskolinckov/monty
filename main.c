5ef8d3b630fd4b3c3c19b159a45c9abecd00dfc0
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2023-03-25 17:05:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,29 @@
 #include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
 
 /**
- * main - Main function
- * @argc: Arguments counter
- * @argv: Arguments vector
- * Return: Default return
- **/
-
-int main(int argc, char *argv[])
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
 {
-	char *tokens, *lineptr = NULL;
-	unsigned int counter = 0;
-	stack_t *head = NULL;
-	ssize_t read_file;
-	size_t bufsize;
-	FILE *fl;
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fl = fopen(argv[1], "r");
-	if (!fl)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	while ((read_file = getline(&lineptr, &bufsize, fl)) != -1)
-	{
-		tokens = strtok(lineptr, LIMITER);
-		counter++;
-		if (tokens)
-			select_func(&head, tokens, counter);
-
-		else if (!tokens)
-			continue;
-
-	}
-	if (lineptr)
-		free(lineptr);
-	free_all(&head);
-	fclose(fl);
-	exit(EXIT_SUCCESS);
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
